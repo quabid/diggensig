0d21897af9ca5893de2772a3edb8304184e2a949
quabid <quabidlord@gmail.com>
2021-03-06 11:31:00 -0500

    Abstracted Verifier class

--- siggenerator/src/main/java/com/gmail/quabidlord/App.java
+++ siggenerator/src/main/java/com/gmail/quabidlord/App.java
@@ -11,7 +11,6 @@ import com.gmail.quabidlord.pathmanager.MyConstants;
 public class App {
     final static PrintStream printer = new PrintStream(System.out);
     static String[] providers;
-    final static Generator generator = new Generator();
     final static MyConstants constants = new MyConstants();
     final static String pathToSaveTheSignatureFile = constants.USRHOME + "sig";
     final static String pathToSaveThePublicKeyFile = constants.USRHOME + "pubKey";
@@ -22,32 +21,39 @@ public class App {
     final static String dsaAlgorithm = "SHA1withDSA";
     final static int keySize = 1024;
 
+    /**
+     * 
+     * @param pathToFileDataToGenerateSignatureFrom
+     * @param pathToSaveTheSignatureFile
+     * @param pathToSaveThePublicKeyFile
+     * @param pathToSaveThePrivateKeyFile
+     * @param provider
+     * @param signatureAlgorithm
+     * @param dsaAlgorithm
+     * @param keySize
+     */
+    final static Generator generator = new Generator();
+
+    /**
+     * @param pathToThePublicKey
+     * @param pathToTheSignatureFile
+     * @param pathToTheDataFile
+     */
+    final static Verifier verifier = new Verifier();
+
     public static void main(String[] args) {
-        /**
-         * 
-         * @param pathToFileDataToGenerateSignatureFrom
-         * @param pathToSaveTheSignatureFile
-         * @param pathToSaveThePublicKeyFile
-         * @param pathToSaveThePrivateKeyFile
-         * @param provider
-         * @param signatureAlgorithm
-         * @param dsaAlgorithm
-         * @param keySize
-         */
-        Generator generator = new Generator();
 
-        /**
-         * @param pathToThePublicKey
-         * @param pathToTheSignatureFile
-         * @param pathToTheDataFile
-         */
-        Verifier verifier = new Verifier();
+        if (args.length != 8) {
+            print(String.format(
+                    "\n\tThis program is expecting the following arguments:\n\t%s\n\t%s\n\t%s\n\t%s\n\t%s\n\t%s\n\t%s\n\t%s",
+                    "dataFile", "pathToSaveTheSignatureFile", "pathToSaveThePublicKeyFile",
+                    "pathToSaveThePrivateKeyFile", "provider", "signatureAlgorithm", "dsaAlgorithm", "keySize"));
+        } else {
+            generator.generate(dataFile, pathToSaveTheSignatureFile, pathToSaveThePublicKeyFile,
+                    pathToSaveThePrivateKeyFile, provider, signatureAlgorithm, dsaAlgorithm, keySize);
 
-        // generator.generate("/home/sjhadmin/bin/tooct", "sun");
-        generator.generate(dataFile, pathToSaveTheSignatureFile, pathToSaveThePublicKeyFile,
-                pathToSaveThePrivateKeyFile, provider, signatureAlgorithm, dsaAlgorithm, keySize);
-
-        verifier.verify(pathToSaveThePublicKeyFile, pathToSaveTheSignatureFile, dataFile);
+            verifier.verify(pathToSaveThePublicKeyFile, pathToSaveTheSignatureFile, dataFile);
+        }
 
     }
 
